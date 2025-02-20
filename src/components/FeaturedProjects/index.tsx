45689fc681515cd6bde950342117e7b7bb20d313
Bayu Rizki Rahmadi <bayu.r.rahmadi@gmail.com>
2025-02-21 01:12:06 +0700

    feat: handle featured projects

--- src/components/FeaturedProjects/index.tsx
+++ src/components/FeaturedProjects/index.tsx
@@ -1,24 +1,37 @@
-import Card from "@src/components/Card";
+"use client";
+
+import { Loader } from "lucide-react";
+
+import ProjectCard from "@src/components/ProjectCard";
+import { useGithubUserProjects } from "@src/hooks/useGithubUserProjects";
 
 import styles from "./FeaturedProjects.module.css";
 
 const FeaturedProjects = () => {
+  const { data: projectsData, isLoading } = useGithubUserProjects(
+    "peacemaker14",
+    1,
+    3
+  );
+
+  if (isLoading) {
+    return (
+      <section className={styles.featured}>
+        <h2>Featured Projects</h2>
+        <div className={styles.loading}>
+          <Loader className={styles.loadingIcon} />
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className={styles.featured}>
       <h2>Featured Projects</h2>
       <div className={styles.projectGrid}>
-        <Card
-          title="Project Name"
-          description="Project description goes here"
-        />
-        <Card
-          title="Project Name"
-          description="Project description goes here"
-        />
-        <Card
-          title="Project Name"
-          description="Project description goes here"
-        />
+        {projectsData?.data.map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
       </div>
     </section>
   );
